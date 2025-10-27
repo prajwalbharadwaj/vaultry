8fe694c2561ae9bb6c32e61c09c4785505533f9d
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-10-27 15:36:09 +0530

    feat: page header component for each page

---
+++ src/common/components/PageHeader.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: 'Page Header',
+  },
+  breadcrumb: {
+    type: String,
+    default: 'Page Header',
+  },
+});
+
+// const items = ref([
+//   { label: 'Electronics' },
+//   { label: 'Computer' },
+//   { label: 'Accessories' },
+//   { label: 'Keyboard' },
+//   { label: 'Wireless' },
+// ]);
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <h1 class="text-xl font-bold tracking-tight">
+      {{ title }}
+    </h1>
+    <!-- <PBreadcrumb :model="items" /> -->
+  </div>
+</template>
