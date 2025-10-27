900bc6b94c87f72482e22f34633b4d2d7704275b
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-10-27 18:03:54 +0530

    feat: added z-index for the navbar

--- src/common/components/Navbar.vue
+++ src/common/components/Navbar.vue
@@ -9,7 +9,7 @@ const emit = defineEmits(['toggleMiniSidebar']);
 </script>
 
 <template>
-  <div class="bg-white h-14 w-full flex justify-between items-center px-5 sticky top-0 shadow">
+  <div class="bg-white h-14 w-full flex justify-between items-center px-5 sticky top-0 shadow z-1">
     <PButton icon="pi" severity="secondary" @click="emit('toggleMiniSidebar', !props.isMiniSidebar)">
       <IconTablerMenu2 />
     </PButton>
