1ac214f4ab7db8c72a3d84163e96a53c8cfd1b3f
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-10-27 15:37:30 +0530

    feat: added blank page for quick insertion

---
+++ src/views/blank/blank.vue
@@ -0,0 +1,10 @@
+<script setup>
+import PageHeader from '~/common/components/PageHeader.vue';
+</script>
+
+<template>
+  <div class="p-5 grid gap-5">
+    <PageHeader title="Page title" />
+    <!-- content -->
+  </div>
+</template>
