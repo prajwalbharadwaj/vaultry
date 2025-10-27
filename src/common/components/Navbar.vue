9b3a7b247bb36fdd92bcaf400dd3a425c3f91075
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-10-27 12:38:33 +0530

    feat: navbar updated

--- src/common/components/Navbar.vue
+++ src/common/components/Navbar.vue
@@ -1,12 +1,17 @@
 <script setup>
-import { useDark, useToggle } from '@vueuse/core';
-
-const isDark = useDark();
-const toggleDark = useToggle(isDark);
+const props = defineProps({
+  isMiniSidebar: {
+    type: Boolean,
+    default: false,
+  },
+});
+const emit = defineEmits(['toggleMiniSidebar']);
 </script>
 
 <template>
-  <div>
-    <span @click="toggleDark()">change theme</span>
+  <div class="bg-white h-14 w-full flex justify-between items-center px-5">
+    <PButton icon="pi" severity="secondary" @click="emit('toggleMiniSidebar', !props.isMiniSidebar)">
+      <IconTablerMenu2 />
+    </PButton>
   </div>
 </template>
