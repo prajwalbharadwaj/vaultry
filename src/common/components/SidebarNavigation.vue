b865fe9eb255bad6c27b5ca8716d602aed2ec339
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-12-04 12:11:04 +0530

    updated the icons in sidebar navigation

--- src/common/components/SidebarNavigation.vue
+++ src/common/components/SidebarNavigation.vue
@@ -1,5 +1,12 @@
 <script setup>
+import IconTablerCalendarEvent from '~icons/tabler/calendar-event';
+import IconTablerChartBar from '~icons/tabler/chart-bar';
+import IconTablerCurrencyDollar from '~icons/tabler/currency-dollar';
 import IconTablerHome from '~icons/tabler/home';
+import IconTablerPigMoney from '~icons/tabler/pig-money';
+import IconTablerSettings from '~icons/tabler/settings';
+import IconTablerTargetArrow from '~icons/tabler/target-arrow';
+import IconTablerTransfer from '~icons/tabler/transfer';
 import { useCommonImports } from '~/common/composables/common-imports.composable.js';
 
 defineProps({
@@ -8,16 +15,17 @@ defineProps({
     default: false,
   },
 });
-const { route,router } = useCommonImports();
+const { route, router } = useCommonImports();
 const navigation = [
+
   { name: 'Dashboard', to: 'dashboard', icon: IconTablerHome },
-  { name: 'Transactions', to: 'transactions', icon: IconTablerHome },
-  { name: 'Stats', to: 'stats', icon: IconTablerHome },
-  { name: 'Budget', to: 'budget', icon: IconTablerHome },
-  { name: 'Goals', to: 'goals', icon: IconTablerHome },
-  { name: 'Loans', to: 'loans', icon: IconTablerHome },
-  { name: 'Schedule', to: 'schedule', icon: IconTablerHome },
-  { name: 'Settings', to: 'settings', icon: IconTablerHome },
+  { name: 'Transactions', to: 'transactions', icon: IconTablerTransfer },
+  { name: 'Stats', to: 'stats', icon: IconTablerChartBar },
+  { name: 'Budget', to: 'budget', icon: IconTablerPigMoney },
+  { name: 'Goals', to: 'goals', icon: IconTablerTargetArrow },
+  { name: 'Loans', to: 'loans', icon: IconTablerCurrencyDollar },
+  { name: 'Schedule', to: 'schedule', icon: IconTablerCalendarEvent },
+  { name: 'Settings', to: 'settings', icon: IconTablerSettings },
 ];
 </script>
 
@@ -28,7 +36,7 @@ const navigation = [
     </div>
     <nav class="flex flex-1 flex-col mt-2" aria-label="Sidebar">
       <ul role="list" class="-mx-2 space-y-1">
-        <li v-for="item in navigation" :key="item.name" @click="router.push({name:item.to})">
+        <li v-for="item in navigation" :key="item.name" @click="router.push({ name: item.to })">
           <a
             v-tooltip="isMiniSidebar ? item.name : ''"
             class="group flex gap-x-3 rounded-md p-2 pl-3 text-sm/6 font-semibold cursor-pointer items-center"
